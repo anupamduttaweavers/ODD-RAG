db3a21245daf5027191a923ef6dbc9f645064ade
Debraj Das <debraj.das@weavers-web.com>
2026-02-06 12:37:32 +0530

    fixed the response issue in the vectore chunks

--- app/api/v1/endpoints/vectore_db.py
+++ app/api/v1/endpoints/vectore_db.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,HTTPException
+from fastapi.responses import JSONResponse
 from app.vectorstore.operations import get_total_docs_count
 
 router = APIRouter()
@@ -8,7 +9,11 @@ router = APIRouter()
 @router.get("/get_total_chunks/")
 async def get_total_chunks():
     """Get the total number of documents in the vector"""
-    count = get_total_docs_count()
-    return {
-        "total_chunks": count
-    }
+    try:
+        count = get_total_docs_count()
+        return JSONResponse(status_code=200,content={"total_chunks": count})
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail=f"Failed to get total chunks: {str(e)}"
+        )
